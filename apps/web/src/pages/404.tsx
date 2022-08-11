1ab413cfdc445e59eddb36ae2612db3e9d3ade4a
Ntermast <gabynteramast@gmail.com>
2022-08-11 14:41:21 +0200

    Feat: layout base structure

--- apps/web/src/pages/404.tsx
+++ apps/web/src/pages/404.tsx
@@ -1,13 +1,11 @@
-import * as React from 'react';
 import { RiAlarmWarningFill } from 'react-icons/ri';
 
-import Layout from '@/components/layout/Layout';
 import ArrowLink from '@/components/links/ArrowLink';
 import Seo from '@/components/Seo';
 
 export default function NotFoundPage() {
   return (
-    <Layout>
+    <>
       <Seo templateTitle='Not Found' />
 
       <main>
@@ -24,6 +22,6 @@ export default function NotFoundPage() {
           </div>
         </section>
       </main>
-    </Layout>
+    </>
   );
 }
--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -4,10 +4,12 @@ const { fontFamily } = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
-        primary: ['Inter', ...fontFamily.sans],
+        primary: ['Montserrat', 'sans-serif'],
+        // primary: ['Inter', ...fontFamily.sans],
       },
       colors: {
         primary: {
