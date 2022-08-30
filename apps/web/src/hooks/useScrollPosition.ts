b9a4ba075192ba042bd3612ce19d17d9d5b500c2
Ntermast <gabynteramast@gmail.com>
2022-08-31 00:16:16 +0200

    feat: useScrollPosition custom hook

---
+++ apps/web/src/hooks/useScrollPosition.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react"
+
+export const useScrollPosition = () => {
+    const [scrollPosition, setScrollPosition] = useState<number>(0)
+
+    const updatePosition = () => {
+            setScrollPosition(window.pageYOffset);
+        };
+
+    useEffect(() => {
+
+        window.addEventListener('scroll', updatePosition);
+
+        updatePosition();
+
+        return () => window.removeEventListener('scroll', updatePosition);
+    },[])
+  return scrollPosition;
+}
