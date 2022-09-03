6f1a5b922fcca3676f6e30250c032f8eb2af6347
Ntermast <gabynteramast@gmail.com>
2022-09-03 11:59:24 +0200

    feat: footer code refactoring

---
+++ apps/web/src/types/footerTypes.ts
@@ -0,0 +1,13 @@
+export type IFooterItemLinks = {
+    bigTitle: string,
+    childItems: {
+      smallTitle: string,
+      href: string
+    }[]
+  }
+  
+export type ISocialIcon = {
+    icon: JSX.Element,
+    href: string,
+  }
+  
