a951156172520eb7198a57cf2aae5e216e1f6792
paradoxe35 <35149259+paradoxe35@users.noreply.github.com>
2022-09-08 16:55:35 +0200

    * Fix build error (Do not use "@ts-ignore")
    * Cms snapshot

--- apps/web/src/constant/cms.ts
+++ apps/web/src/constant/cms.ts
@@ -7,14 +7,12 @@ type WithTranslation<T> = T & {
   [k in keyof T as `${string & k}_translations`]: T[k];
 };
 
-function modelsWithTranslation<T extends { [x: string]: string }>(
-  models: T
-): WithTranslation<T> {
-  for (const key in models) {
-    //@ts-ignore
-    models[`${key}_translations`] = `${models[key]}_translations`;
+function modelsWithTranslation<T>(models: T): WithTranslation<T> {
+  const $models: any = models;
+  for (const key in $models) {
+    $models[`${key}_translations`] = `${$models[key]}_translations`;
   }
-  return models as WithTranslation<T>;
+  return $models as WithTranslation<T>;
 }
 
 export const CMS_MODELS = modelsWithTranslation({
