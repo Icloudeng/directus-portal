4bd3adfeb27a60182717f9b498f65e3c31b1810c
Ntermast <gabynteramast@gmail.com>
2022-09-13 23:01:12 +0200

    fix: cloud computing responsiveness

--- apps/web/src/components/sections/cloudComputing/CloudComputing.tsx
+++ apps/web/src/components/sections/cloudComputing/CloudComputing.tsx
@@ -1,157 +1,52 @@
 /* eslint-disable react/no-unescaped-entities */
 
-import { Carousel } from "flowbite-react"
-
-import ButtonLink from "@/components/links/ButtonLink"
-import NextImage from "@/components/NextImage"
-
 import { CarouselItem } from "./components/CarouselItem"
+
+
 
 export const CloudComputing = () => {
 
     return (
-        <div className="container max-w-7xl mx-auto py-10 flex flex-col items-center gap-10">
+        <div className="x-container max-w-7xl mx-auto py-10 flex flex-col items-center gap-10 px-12">
             <div className="flex flex-col items-center justify-center gap-7 mb-7">
                 <h1>What's Cloud Computing</h1>
                 <span className="max-w-xl text-center">Managing a cloud infrastructure has never been so enjoyable. It's lightning-fast and stunning simple.</span>
             </div>
-            <div className="w-full h-[29rem]">
-                <Carousel slideInterval={5000}>
-                    <div className="container__block max-w-5xl w-full grid grid-cols-1 sd:grid-cols-2 gap-16 lg:flex items-center justify-center lg:gap-10 px-4">
-                        <div className="card-wrapper w-full">
-                            <div className="w-full flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row hover:bg-gray-50">
-                                <div className="relative flex flex-[1.2] flex-col justify-between p-4">
-                                    <NextImage
-                                        useSkeleton
-                                        className="object-cover w-full h-[27rem] rounded-t-lg md:rounded-sm md:rounded-l-lg"
-                                        src="https://flowbite.com/docs/images/blog/image-1.jpg"
-                                        layout="fill"
-                                        width={100}
-                                        objectFit="cover"
-                                        alt='hero banner image'
-                                    />
-                                </div>
-                                <div className="flex flex-[2] flex-col justify-between p-16 leading-normal">
-                                    <div className="mb-7">
-                                        <h5 className="mb-3 text-2xl font-bold tracking-tight text-gray-900">Noteworthy technology acquisitions 2021</h5>
-                                        <p className="mb-3 font-normal text-gray-700">Explain the importance of IT System in a company</p>
-                                    </div>
-                                    <div className="">
-                                        <h6 className="mb-3 text-lg font-semibold tracking-tight text-gray-900">Noteworthy technology</h6>
-                                        <ul className="list-disc ml-5 mb-7">
-                                            <li>Large clouds often have functions</li>
-                                            <li>Here are the biggest enterprise technology</li>
-                                            <li>platform on which applications and systems</li>
-                                            <li>Cloud computing is the delivery of different services</li>
-                                        </ul>
-                                        <ButtonLink variant='outline' href="#" className="float-right text-center text-sm font-base rounded-sm">Read More</ButtonLink>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+            <div className="w-full">
 
+                <div id="default-carousel" className="relative" data-carousel="static">
+                    {/* <!-- Carousel wrapper --> */}
+                    <div className="relative overflow-hidden rounded-lg h-[47rem] ss:h-[40rem] sm:h-[30rem]">
+                        {/* <!-- Item 1 --> */}
+                        <CarouselItem />
+                        {/* <!-- Item 2 --> */}
+                        <CarouselItem />
+                        {/* <!-- Item 3 --> */}
+                        <CarouselItem />
                     </div>
-                    <div className="container__block max-w-5xl w-full grid grid-cols-1 sd:grid-cols-2 gap-16 lg:flex items-center justify-center lg:gap-10 px-4">
-                        <div className="card-wrapper w-full">
-                            <div className="w-full flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row hover:bg-gray-50">
-                                <div className="relative flex flex-[1.2] flex-col justify-between p-4">
-                                    <NextImage
-                                        useSkeleton
-                                        className="object-cover w-full h-[27rem] rounded-t-lg md:rounded-sm md:rounded-l-lg"
-                                        src="https://flowbite.com/docs/images/blog/image-2.jpg"
-                                        layout="fill"
-                                        width={100}
-                                        objectFit="cover"
-                                        alt='hero banner image'
-                                    />
-                                </div>
-                                <div className="flex flex-[2] flex-col justify-between p-16 leading-normal">
-                                    <div className="mb-7">
-                                        <h5 className="mb-3 text-2xl font-bold tracking-tight text-gray-900">consumption-based pricing model</h5>
-                                        <p className="mb-3 font-normal text-gray-700">Explain what problems occur in companies when IT System are on premise.</p>
-                                    </div>
-                                    <div className="">
-                                        <h6 className="mb-3 text-lg font-semibold tracking-tight text-gray-900">Noteworthy technology</h6>
-                                        <ul className="list-disc ml-5 mb-7">
-                                            <li>functions distributed over multiple locations</li>
-                                            <li>Here are the biggest enterprise technology</li>
-                                            <li>each location being a data center</li>
-                                            <li>Noteworthy technology acquisitions</li>
-                                        </ul>
-                                        <ButtonLink variant='outline' href="#" className="float-right text-center text-sm font-base rounded-sm">Read More</ButtonLink>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                    {/* <!-- Slider indicators --> */}
+                    <div className="absolute z-30 flex space-x-3 -translate-x-1/2 -bottom-10 left-1/2">
+                        <button type="button" className="w-3 h-3 rounded-full bg-primary-300 " aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
+                        <button type="button" className="w-3 h-3 rounded-full bg-gray-300/50 hover:bg-gray-300" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
+                        <button type="button" className="w-3 h-3 rounded-full bg-gray-300/50 hover:bg-gray-300" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
                     </div>
-                    <div className="container__block max-w-5xl w-full grid grid-cols-1 sd:grid-cols-2 gap-16 lg:flex items-center justify-center lg:gap-10 px-4">
-                        <div className="card-wrapper w-full">
-                            <div className="w-full flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row hover:bg-gray-50">
-                                <div className="relative flex flex-[1.2] flex-col justify-between p-4">
-                                    <NextImage
-                                        useSkeleton
-                                        className="object-cover w-full h-[27rem] rounded-t-lg md:rounded-sm md:rounded-l-lg"
-                                        src="https://flowbite.com/docs/images/blog/image-4.jpg"
-                                        layout="fill"
-                                        width={100}
-                                        objectFit="cover"
-                                        alt='hero banner image'
-                                    />
-                                </div>
-                                <div className="flex flex-[2] flex-col justify-between p-16 leading-normal">
-                                    <div className="mb-7">
-                                        <h5 className="mb-3 text-2xl font-bold tracking-tight text-gray-900">Dig Deeper on Cloud infrastructure design and management</h5>
-                                        <p className="mb-3 font-normal text-gray-700">Explain solutions of problems the moving of IT system in the cloud solves and how it ends all those problems.</p>
-                                    </div>
-                                    <div className="">
-                                        <h6 className="mb-3 text-lg font-semibold tracking-tight text-gray-900">Noteworthy technology</h6>
-                                        <ul className="list-disc ml-5 mb-7">
-                                            <li>Cloud computing is the on-demand availability</li>
-                                            <li>Here are the biggest enterprise technology</li>
-                                            <li>portal Lorem ipsum dolor sit</li>
-                                            <li>without direct active management by the user</li>
-                                        </ul>
-                                        <ButtonLink variant='outline' href="#" className="float-right text-center text-sm font-base rounded-sm">Read More</ButtonLink>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="container__block max-w-5xl w-full grid grid-cols-1 sd:grid-cols-2 gap-16 lg:flex items-center justify-center lg:gap-10 px-4">
-                        <div className="card-wrapper w-full">
-                            <div className="w-full flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row hover:bg-gray-50">
-                                <div className="relative flex flex-[1.2] flex-col justify-between p-4">
-                                    <NextImage
-                                        useSkeleton
-                                        className="object-cover w-full h-[27rem] rounded-t-lg md:rounded-sm md:rounded-l-lg"
-                                        src="https://images.unsplash.com/photo-1577760258779-e787a1733016?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-                                        layout="fill"
-                                        width={100}
-                                        objectFit="cover"
-                                        alt='hero banner image'
-                                    />
-                                </div>
-                                <div className="flex flex-[2] flex-col justify-between p-16 leading-normal">
-                                    <div className="mb-7">
-                                        <h5 className="mb-3 text-2xl font-bold tracking-tight text-gray-900">What is public cloud? Everything you need to know</h5>
-                                        <p className="mb-3 font-normal text-gray-700">Explain the global advantages of cloud computing for companies and structure.</p>
-                                    </div>
-                                    <div className="">
-                                        <h6 className="mb-3 text-lg font-semibold tracking-tight text-gray-900">Noteworthy technology</h6>
-                                        <ul className="list-disc ml-5 mb-7">
-                                            <li>Simply put, cloud computing is the delivery</li>
-                                            <li>Here are the biggest enterprise technology</li>
-                                            <li>(“the cloud”) to offer faster innovation</li>
-                                            <li>Noteworthy technology acquisitions</li>
-                                        </ul>
-                                        <ButtonLink variant='outline' href="#" className="float-right text-center text-sm font-base rounded-sm">Read More</ButtonLink>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </Carousel>
+                    {/* <!-- Slider controls --> */}
+                    <button type="button" className="absolute top-0 -left-16 sm:-left-20 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev="">
+                        <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 group-hover:bg-white/50 ring-2 group-focus:ring-3 group-focus:ring-primary-400 group-focus:outline-none">
+                            <svg aria-hidden="true" className="w-5 h-5 text-primary-400 sm:w-6 sm:h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7"></path></svg>
+                            <span className="sr-only">Previous</span>
+                        </span>
+                    </button>
+                    <button type="button" className="absolute top-0 -right-16 sm:-right-20 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next="">
+                        <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 group-hover:bg-white/50 ring-2 group-focus:ring-3 group-focus:ring-primary-400 group-focus:outline-none">
+                            <svg aria-hidden="true" className="w-5 h-5 text-primary-400 sm:w-6 sm:h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>
+                            <span className="sr-only">Next</span>
+                        </span>
+                    </button>
+                </div>
+
             </div>
         </div>
+
     )
 }
--- apps/web/src/components/sections/cloudComputing/components/CarouselItem.tsx
+++ apps/web/src/components/sections/cloudComputing/components/CarouselItem.tsx
@@ -1,34 +1,37 @@
+import Image from "next/image"
+
 import ButtonLink from "@/components/links/ButtonLink"
-import NextImage from "@/components/NextImage"
 
 export const CarouselItem = () => {
     return (
-        <div className="card-wrapper w-full">
-            <div className="w-full flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row hover:bg-gray-50">
-                <div className="relative flex flex-[1.2] flex-col justify-between p-4">
-                    <NextImage
-                        useSkeleton
-                        className="object-cover w-full h-[27rem] rounded-t-lg md:rounded-sm md:rounded-l-lg"
-                        src="https://flowbite.com/docs/images/blog/image-4.jpg"
+        <div className="duration-700 ease-in-out absolute inset-0 transition-all transform translate-x-0 z-20" data-carousel-item="">
+            <div className="w-full h-full flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row hover:bg-gray-50">
+                <div className="image-container relative md:h-[31rem] w-full flex flex-[1.2] flex-col justify-between p-24 md:p-4">
+                    <Image
+                        // useSkeleton
+                        className="image object-cover  rounded-t-lg md:rounded-sm md:rounded-l-lg"
+                        src="https://images.unsplash.com/photo-1577760258779-e787a1733016?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
                         layout="fill"
-                        width={100}
+                        // width={200}
                         objectFit="cover"
                         alt='hero banner image'
                     />
                 </div>
-                <div className="flex flex-[2] flex-col justify-between p-16 leading-normal">
-                    <div className="mb-7">
-                        <h5 className="mb-3 text-2xl font-bold tracking-tight text-gray-900">Noteworthy technology acquisitions 2021</h5>
-                        <p className="mb-3 font-normal text-gray-700">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <div className="flex flex-[2] flex-col justify-between p-5 md:p-16 leading-normal">
+                    <div className="mb-4 sm:mb-7">
+                        <h5 className="mb-3 text-2xl font-bold tracking-tight text-gray-900">What is public cloud? Everything you need to know</h5>
+                        <p className="mb-3 font-normal text-gray-700">Explain the global advantages of cloud computing for companies and structure.</p>
                     </div>
-                    <div className="">
-                        <h6 className="mb-3 text-lg font-semibold tracking-tight text-gray-900">Noteworthy technology</h6>
-                        <ul className="list-disc ml-5 mb-7">
-                            <li>suscipit deleniti facilis</li>
-                            <li>Here are the biggest enterprise technology</li>
-                            <li>portal Lorem ipsum dolor sit</li>
-                            <li>Noteworthy technology acquisitions</li>
-                        </ul>
+                    <div className="flex flex-col sd:flex-row items-center justify-between md:block">
+                        <div className="mb-7">
+                            <h6 className="mb-3 text-center sd:text-start text-lg font-semibold tracking-tight text-gray-900">Noteworthy technology</h6>
+                            <ul className="list-disc ml-5 text-xs sd:text-sm text-gray-400">
+                                <li>Simply put, cloud computing is the delivery</li>
+                                <li>Here are the biggest enterprise technology</li>
+                                <li>(“the cloud”) to offer faster innovation</li>
+                                <li>Noteworthy technology acquisitions</li>
+                            </ul>
+                        </div>
                         <ButtonLink variant='outline' href="#" className="float-right text-center text-sm font-base rounded-sm">Read More</ButtonLink>
                     </div>
                 </div>
