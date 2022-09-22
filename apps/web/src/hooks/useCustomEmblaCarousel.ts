f8200482e5bcef404dc7e82dbf6c55e9024181ba
paradoxe35 <35149259+paradoxe35@users.noreply.github.com>
2022-09-22 21:26:15 +0200

    minor changes

--- apps/web/src/hooks/useCustomEmblaCarousel.ts
+++ apps/web/src/hooks/useCustomEmblaCarousel.ts
@@ -1,33 +1,43 @@
 import useEmblaCarousel from 'embla-carousel-react';
 import { useCallback, useEffect, useState } from 'react';
 
-export const useCustomerEmblaCarousel = () => {
-    const [viewportRef, embla] = useEmblaCarousel({ loop: false });
+export const useCustomerEmblaCarousel = (startIndex?: number) => {
+  const [viewportRef, embla] = useEmblaCarousel({ loop: false, startIndex });
 
-    const scrollPrev = useCallback(() => embla && embla.scrollPrev(), [embla]);
-    const scrollNext = useCallback(() => embla && embla.scrollNext(), [embla]);
-    const [prevBtnEnabled, setPrevBtnEnabled] = useState(false);
-    const [nextBtnEnabled, setNextBtnEnabled] = useState(false);
-    const [selectedIndex, setSelectedIndex] = useState(0);
-    const [scrollSnaps, setScrollSnaps] = useState([]);
+  const scrollPrev = useCallback(() => embla && embla.scrollPrev(), [embla]);
+  const scrollNext = useCallback(() => embla && embla.scrollNext(), [embla]);
+  const [prevBtnEnabled, setPrevBtnEnabled] = useState(false);
+  const [nextBtnEnabled, setNextBtnEnabled] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [scrollSnaps, setScrollSnaps] = useState([]);
 
-    const scrollTo = useCallback((index: number) => embla && embla.scrollTo(index), [
-        embla
-    ]);
+  const scrollTo = useCallback(
+    (index: number) => embla && embla.scrollTo(index),
+    [embla]
+  );
 
-    const onSelect = useCallback(() => {
-        if (!embla) return;
-        setSelectedIndex(embla.selectedScrollSnap());
-        setPrevBtnEnabled(embla.canScrollPrev());
-        setNextBtnEnabled(embla.canScrollNext());
-    }, [embla, setSelectedIndex]);
+  const onSelect = useCallback(() => {
+    if (!embla) return;
+    setSelectedIndex(embla.selectedScrollSnap());
+    setPrevBtnEnabled(embla.canScrollPrev());
+    setNextBtnEnabled(embla.canScrollNext());
+  }, [embla, setSelectedIndex]);
 
-    useEffect(() => {
-        if (!embla) return;
-        embla.on('select', onSelect);
-        onSelect();
-        setScrollSnaps(embla.scrollSnapList() as any);
-    }, [embla, setScrollSnaps, onSelect]);
+  useEffect(() => {
+    if (!embla) return;
+    embla.on('select', onSelect);
+    onSelect();
+    setScrollSnaps(embla.scrollSnapList() as any);
+  }, [embla, setScrollSnaps, onSelect]);
 
-    return {viewportRef, scrollPrev, scrollNext, prevBtnEnabled, nextBtnEnabled, selectedIndex, scrollSnaps, scrollTo}
-}
+  return {
+    viewportRef,
+    scrollPrev,
+    scrollNext,
+    prevBtnEnabled,
+    nextBtnEnabled,
+    selectedIndex,
+    scrollSnaps,
+    scrollTo,
+  };
+};
