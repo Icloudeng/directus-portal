ab63a44ce8166afe69290367bcaca770c84e6380
Ntermast <gabynteramast@gmail.com>
2022-09-27 12:09:50 +0200

    feat:responsiveness and scroll add indicator

--- apps/web/src/components/sections/support-business/components/Accordion.tsx
+++ apps/web/src/components/sections/support-business/components/Accordion.tsx
@@ -1,3 +1,4 @@
+import { DotButton } from '@/components/carouselButtons/CarouselButtons';
 import Skeleton from '@/components/Skeleton'
 import React, { FunctionComponent, PropsWithChildren, useState } from 'react'
 
@@ -51,7 +52,7 @@ export const Accordion: FunctionComponent<PropsWithChildren<Props>> = ({ childre
                         <div className="accordion__list-contents w-full text-content flex-[1.3] flex flex-col items-start gap-7 sm:border-l-[1px]">
                             {titles?.map(({ title, description, key }) => {
                                 const active = activeKey === key
-                                return <div key={key} className={`transition-all accordion__list-contents-texts w-full flex-1 ${active ? 'flex sm:border-l-[1px] border-primary-400' : 'sm:flex'} flex-col items-center sm:items-start gap-5 pl-7`}>
+                                return <div key={key} className={`transition-all accordion__list-contents-texts w-full flex-1 ${active ? 'flex sm:border-l-[1px] border-primary-400' : 'hidden sm:flex'} flex-col items-center sm:items-start gap-5 pl-7`}>
                                     <h3 onClick={() => setActiveKey(key)} className={`transition-all cursor-pointer ${active ? 'text-black' : 'text-black/60'} hover:text-black`}>{title}</h3>
                                     <span className={`transition-all sm:max-w-sm text-center sm:text-start ${!active && 'hidden'}`}>
                                         {description}
@@ -72,32 +73,39 @@ export const Accordion: FunctionComponent<PropsWithChildren<Props>> = ({ childre
                     </div>
                 </div>
 
-                <div className="relative float-right mt-10">
-                    <button
-                        type='button'
-                        onClick={nextHandle}
-                        disabled={!hasNext}
-                        className='absolute -left-[3.5rem] flex items-center justify-center px-4 cursor-pointer group focus:outline-none'
-                        data-carousel-prev=''>
-                        <span className={`inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 group-hover:bg-white/50 ring-1 group-focus:ring-3 group-focus:ring-primary-400 group-focus:outline-none ${!hasNext && 'ring-gray-200 '}`}>
-                            <svg aria-hidden='true' className={`w-5 h-5 text-primary-400 sm:w-6 sm:h-6 ${!hasNext && 'text-gray-200 '}`} fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
-                                <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M9 5l7 7-7 7'></path>
-                            </svg>
-                            <span className='sr-only'>Next</span>
-                        </span>
-                    </button>
-
+                <div className="w-full flex sm:hidden items-center justify-between ss:justify-evenly float-right mt-10">
                     <button
                         type='button'
                         onClick={prevHandle}
                         disabled={!hasPrev}
-                        className='absolute right-10 flex items-center justify-center px-4 cursor-pointer group focus:outline-none'
+                        className='flex items-center justify-center px-4 cursor-pointer group focus:outline-none'
                         data-carousel-next=''>
                         <span className={`inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 group-hover:bg-white/50 ring-1 group-focus:ring-3 group-focus:ring-primary-400 group-focus:outline-none ${!hasPrev && 'ring-gray-200 '}`}>
                             <svg aria-hidden='true' className={`w-5 h-5 text-primary-400 sm:w-6 sm:h-6 ${!hasPrev && 'text-gray-200 '}`} fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
                                 <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M15 19l-7-7 7-7'></path>
                             </svg>
                             <span className='sr-only'>Previous</span>
+                        </span>
+                    </button>
+
+                    {/* <!-- Slider indicators --> */}
+                    <div className='flex space-x-3'>
+                        {titles?.map(({ key }) => {
+                            return <DotButton key={key} position={key} selected={activeKey === key} onClick={() => setActiveKey(key)} />
+                        })}
+                    </div>
+
+                    <button
+                        type='button'
+                        onClick={nextHandle}
+                        disabled={!hasNext}
+                        className='flex items-center justify-center px-4 cursor-pointer group focus:outline-none'
+                        data-carousel-prev=''>
+                        <span className={`inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 group-hover:bg-white/50 ring-1 group-focus:ring-3 group-focus:ring-primary-400 group-focus:outline-none ${!hasNext && 'ring-gray-200 '}`}>
+                            <svg aria-hidden='true' className={`w-5 h-5 text-primary-400 sm:w-6 sm:h-6 ${!hasNext && 'text-gray-200 '}`} fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+                                <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M9 5l7 7-7 7'></path>
+                            </svg>
+                            <span className='sr-only'>Next</span>
                         </span>
                     </button>
                 </div>
