bd1d9ff2b6c8e34a18e1dcab9474c0d8fa33791e
paradoxe35 <35149259+paradoxe35@users.noreply.github.com>
2022-10-13 11:26:22 +0200

    fixed: build error

--- apps/web/src/utils/curry.ts
+++ apps/web/src/utils/curry.ts
@@ -1,19 +1,19 @@
-export function curry(func: Function, arity?: number) {
-    return function curried(...args) {
-        if (args.length >= (arity || func.length)) {
-            return func.apply(this, args)
-        }
-        return curried.bind(this, ...args)
+export function curry(func: (...args: any) => any, arity?: number) {
+  return function curried(this: any, ...args: any[]): any {
+    if (args.length >= (arity || func.length)) {
+      return func.apply(this, args);
     }
+    return curried.bind(this, ...args);
+  };
 }
 
-export function curryRight(func: Function, arity?: number) {
-    return function curried(...args) {
-        if (args.length >= (arity || func.length)) {
-            return func.apply(this, args)
-        }
-        return function (...args2) {
-            return curried.apply(this, [...args2, ...args])
-        }
+export function curryRight(func: (...args: any) => any, arity?: number) {
+  return function curried(this: any, ...args: any[]) {
+    if (args.length >= (arity || func.length)) {
+      return func.apply(this, args);
     }
+    return function (this: any, ...args2: any) {
+      return curried.apply(this, [...args2, ...args]);
+    };
+  };
 }
