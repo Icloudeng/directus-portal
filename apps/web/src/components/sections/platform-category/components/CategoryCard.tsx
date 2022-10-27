b655be6512341690b474924fec6b1743f5cc0ee4
Ntermast <gabynteramast@gmail.com>
2022-10-27 15:56:22 +0200

    category card

--- apps/web/src/components/sections/platform-category/components/CategoryCard.tsx
+++ apps/web/src/components/sections/platform-category/components/CategoryCard.tsx
@@ -1,6 +1,48 @@
+import Image from "next/image"
+import { FaRegShareSquare } from "react-icons/fa"
+import { HiOutlineDotsHorizontal } from "react-icons/hi"
 
-export const CategoryCard = () => {
+import UnstyledLink from "@/components/ui/links/UnstyledLink"
+
+type ICategoryCard = {
+  imgLink: string;
+  title: string;
+  text: string;
+  color: string;
+  cardLink: string;
+  cardWidth?: string
+}
+
+export const CategoryCard = ({ imgLink, title, text, color, cardLink, cardWidth  }:ICategoryCard) => {
   return (
-    <div>CategoryCard</div>
+    <div className={`relative max-w-3xl w-full min-h-[25rem] ${cardWidth} bg-white rounded-lg border border-gray-200 shadow-md`}>
+      <UnstyledLink href={cardLink} className={`w-full h-full flex flex-col items-center rounded-lg ${'bg-'+color+'-300'}`}>
+        <div className="relative w-full h-1/2 flex-[1.5]">
+          <Image
+            className='image object-cover rounded-tl-lg rounded-tr-lg'
+            src={imgLink}
+            layout="fill"
+            objectFit="cover"
+            alt='accordion image'
+          />
+        </div>
+        <div className="relative p-5 flex-1">
+          <h3 className=" font-bold tracking-tight text-gray-900">{title}</h3>
+
+          <p className=" font-normal text-gray-700">{text}</p>
+        </div>
+      </UnstyledLink>
+      <div className="absolute top-0 right-0 p-2 flex items-center gap-2">
+        <span className="p-2 bg-white hover:bg-white/90 rounded-full cursor-pointer">
+          <svg width="16" height="16"><path d="m10.12 10.83 4.03 4.02a.5.5 0 0 0 .7-.7l-13-13a.5.5 0 1 0-.7.7l3.23 3.23a6.7 6.7 0 0 0-2.3 3.08l-.05.15-.01.06s-.08.5.35.61a.5.5 0 0 0 .61-.35L3 8.6a3.02 3.02 0 0 1 .2-.52c.16-.34.4-.8.78-1.26.3-.36.66-.72 1.13-1.02l1.57 1.58a2.5 2.5 0 1 0 3.45 3.45Zm-.74-.74a1.5 1.5 0 1 1-1.97-1.97l1.97 1.97Zm-3.06-5.9.85.86C7.43 5.02 7.71 5 8 5c2.04 0 3.29.91 4.03 1.82A5.7 5.7 0 0 1 13 8.6v.02a.5.5 0 0 0 .97-.25v-.02a2.3 2.3 0 0 0-.06-.18 6.7 6.7 0 0 0-1.12-1.98A5.95 5.95 0 0 0 8 4a6.9 6.9 0 0 0-1.68.2Z"></path></svg>
+        </span>
+        <span className="p-2 bg-white hover:bg-white/90 rounded-full cursor-pointer">
+          <HiOutlineDotsHorizontal />
+        </span>
+      </div>
+      <span className="absolute bottom-2 right-2 p-2 bg-green-400 hover:bg-green-400/70 rounded-full cursor-pointer">
+        <FaRegShareSquare className="text-white" />
+      </span>
+    </div>
   )
 }
