e5b357779a2d5cc38848dfe22c9f6df510b37487
paradoxe35 <35149259+paradoxe35@users.noreply.github.com>
2022-11-02 01:31:12 +0200

    fix: dynamic section adapters errors

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -2,13 +2,11 @@ import { GetServerSidePropsContext } from 'next';
 
 import Layout from '@/components/layout/Layout';
 import { CloudComputing } from '@/components/sections/cloud-computing/CloudComputing';
-import { CrossCardAnimation } from '@/components/sections/cross-card-animation/CrossCardAnimation';
 import { HomeHeroSection } from '@/components/sections/hero/HomeHeroSection';
 import { HowEasy } from '@/components/sections/how-easy/HowEasy';
 import { IntelligentSystem } from '@/components/sections/intelligent-system/IntelligentSystem';
 import { JobsCreation } from '@/components/sections/jobs-creation/JobsCreation';
 import { PageSections } from '@/components/sections/page-sections';
-import { QuickTutorials } from '@/components/sections/quick-tutorials/QuickTutorials';
 import { SupportBusiness } from '@/components/sections/support-business/SupportBusiness';
 import Seo from '@/components/Seo';
 
@@ -25,14 +23,6 @@ export default function HomePage(props: Partial<QHomeHeroQueriesType>) {
 
       <section className='hero-section py-10'>
         {HomeHero && <HomeHeroSection data={HomeHero} />}
-      </section>
-
-      <section className='py-10 bg-white'>
-        <CrossCardAnimation />
-      </section>
-
-      <section className='py-10 bg-white'>
-        <QuickTutorials />
       </section>
 
       <section className='py-10 bg-[#f5f7fa]'>
