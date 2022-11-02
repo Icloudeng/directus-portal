c4a65f3f5dae7feede3fa37e03ad09106190f5c9
paradoxe35 <35149259+paradoxe35@users.noreply.github.com>
2022-11-02 02:22:52 +0200

    minor changes

--- apps/web/src/components/ui/cards/RectCard.tsx
+++ apps/web/src/components/ui/cards/RectCard.tsx
@@ -8,24 +8,31 @@ type IRectCard = {
 
 export const RectCard = ({ cardText, cardLogo }: IRectCard) => {
   return (
-    <div className='flex items-center justify-center gap-2 bg-[#f5f7fa] shadow-md w-48 p-6 rounded-lg mr-8'>
-      {cardLogo && (
-        <div className='relative w-7 h-7 rounded-sm'>
-          {typeof cardLogo === 'string' ? (
-            <Image
-              className='image object-cover'
-              src={cardLogo}
-              layout='fill'
-              objectFit='cover'
-              alt='Image'
-              loading='lazy'
-            />
-          ) : (
-            cardLogo
-          )}
-        </div>
-      )}
-      <h3>{cardText ? cardText : ''}</h3>
+    <div className='flex items-center justify-center bg-[#f5f7fa] gap-2 shadow-md max-w-[12rem] min-w-[12rem] p-6 rounded-lg mr-8'>
+      <div>
+        {cardLogo && (
+          <div className='relative w-7 h-7 rounded-sm'>
+            {typeof cardLogo === 'string' ? (
+              <Image
+                className='image object-cover'
+                src={cardLogo}
+                layout='fill'
+                objectFit='cover'
+                alt='Image'
+                loading='lazy'
+              />
+            ) : (
+              cardLogo
+            )}
+          </div>
+        )}
+      </div>
+      <h3
+        className='overflow-hidden text-xl whitespace-nowrap'
+        title={cardText}
+      >
+        {cardText}
+      </h3>
     </div>
   );
 };
