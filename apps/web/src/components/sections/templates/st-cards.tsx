b1882e0c07924c25d18c34b369ef78c228b2a218
paradoxe35 <35149259+paradoxe35@users.noreply.github.com>
2022-11-05 01:59:49 +0200

    fix build error

--- apps/web/src/components/sections/templates/st-cards.tsx
+++ apps/web/src/components/sections/templates/st-cards.tsx
@@ -1,5 +1,5 @@
 import { STemplates_Props, ST_Card } from '@/cms/page-sections';
 
-export function ST_CardsFC({}: STemplates_Props<ST_Card>) {
+export function ST_CardsFC({ items }: STemplates_Props<ST_Card>) {
   return <></>;
 }
