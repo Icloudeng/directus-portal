e1aeddbc1236818f1369bce6ca48b9e41edd3908
Ntermast <gabynteramast@gmail.com>
2022-11-04 21:09:04 +0200

    feat: market place design

--- apps/web/src/components/sections/platform-category/PlatformCategory.tsx
+++ apps/web/src/components/sections/platform-category/PlatformCategory.tsx
@@ -2,6 +2,15 @@ import { AllCategoryItems } from "./components/AllCategoryCards"
 import { AllCategoryCads } from "./components/AllCategoryItems"
 import { CategoryElementsHeader } from "./components/CategoryElementsHeader"
 import { InputWithIcon } from "./components/InputWithIcon"
+import { MarketPlaceCard } from "./components/MarketPlaceCard"
+
+import dockerLogo from '~/images/docker.png';
+import fingerprintLogo from '~/images/fingerprint.png';
+import gitlabLogo from '~/images/gitlab.png';
+import metricLogo from '~/images/metric.png';
+import parabolicLogo from '~/images/parabolic.png';
+import slackLogo from '~/images/slack.png';
+
 
 export const PlatformCategory = () => {
   return (
@@ -24,7 +33,44 @@ export const PlatformCategory = () => {
           </div>
           <div className="xl:flex-[3.7]">
             {/* <CategoryElementsHeader /> */}
-            <AllCategoryCads />
+            <div className="grid grid-cols-3 gap-3">
+              <MarketPlaceCard
+                itemLink="#"
+                itemLogo={dockerLogo}
+                itemName="Docker"
+                description="With 1Password Business, you can automate many common administrative tasks using the 1Password SCIM bridge."
+              />
+              <MarketPlaceCard
+                itemLink="#"
+                itemLogo={gitlabLogo}
+                itemName="Gitlab"
+                description="Shorten development cycles and innovate faster with reliability through DevOps automation."
+              />
+              <MarketPlaceCard
+                itemLink="#"
+                itemLogo={slackLogo}
+                itemName="Slack"
+                description="Slack is a messaging program designed specifically for the office, but has also been adopted for personal use."
+              />
+              <MarketPlaceCard
+                itemLink="#"
+                itemLogo={metricLogo}
+                itemName="Metric"
+                description="The metric system is a system of measurement that succeeded the decimalised system based on the metre."
+              />
+              <MarketPlaceCard
+                itemLink="#"
+                itemLogo={fingerprintLogo}
+                itemName="Fingerprint"
+                description="A fingerprint is an impression left by the friction ridges of a human finger."
+              />
+              <MarketPlaceCard
+                itemLink="#"
+                itemLogo={parabolicLogo}
+                itemName="Parabolic"
+                description="Parabolic usually refers to something in a shape of a parabola, but may also refer to a parable. Parabolic may refer to: In mathematics:."
+              />
+            </div>
           </div>
         </div>
       </div>
