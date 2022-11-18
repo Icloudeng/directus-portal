78a4177d300c5eed60a781bfd02f2fba3f151483
paradoxe35 <35149259+paradoxe35@users.noreply.github.com>
2022-11-18 13:08:59 +0200

    matomo integration

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -11,17 +11,38 @@ import { ISharedData, SharedDataProvider } from '@/app/store';
 
 import { getGqlSharedData } from '@/cms/items';
 import NextProgress from '@/components/ui/next-progress';
+import { useEffect } from 'react';
+import matomoInit from '@/lib/matomo-next';
+
+const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL;
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
+const CHATWOOT_BASE_URL = process.env.NEXT_PUBLIC_CHATWOOT_BASE_URL;
+const CHATWOOT_API_KEY = process.env.NEXT_PUBLIC_CHATWOOT_API_KEY;
 
 function MyApp({
   Component,
   pageProps,
   datas,
 }: AppProps & { datas: ISharedData }) {
+  useEffect(() => {
+    if (MATOMO_URL && MATOMO_SITE_ID) {
+      matomoInit({
+        url: MATOMO_URL,
+        siteId: MATOMO_SITE_ID,
+      });
+    }
+  }, []);
+
   return (
     <SharedDataProvider value={datas}>
-      <Component {...pageProps} />
-      <ChatwootWidget />
       <NextProgress height='2px' options={{ showSpinner: false }} />
+      <Component {...pageProps} />
+      {CHATWOOT_BASE_URL && CHATWOOT_API_KEY && (
+        <ChatwootWidget
+          baseUrl={CHATWOOT_BASE_URL}
+          websiteToken={CHATWOOT_API_KEY}
+        />
+      )}
     </SharedDataProvider>
   );
 }
