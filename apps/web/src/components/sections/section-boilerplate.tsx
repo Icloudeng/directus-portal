5bb18a7d56baefc6ef99fa4902a639a689ebc8ab
Ntermast <gabynteramast@gmail.com>
2022-12-07 18:47:56 +0200

    feat: section boilerplate

---
+++ apps/web/src/components/sections/section-boilerplate.tsx
@@ -0,0 +1,22 @@
+export const SectionBoilerplate = () => {
+    return (
+        <div className='x-container max-w-7xl mx-auto py-10 flex flex-col items-center gap-10 ss:px-12'>
+            <div className='flex flex-col items-center justify-center gap-7 mb-7'>
+                <h1 className='text-center'>Section Boilerplate</h1>
+                <span className='max-w-xl text-center'>
+                    This is the section boilerplate template
+                </span>
+            </div>
+
+            <div className="w-full">
+                <p className="text-xs text-center">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Sit ipsam delectus odio ad cum dolor sint, a temporibus
+                    reprehenderit amet consequuntur quisquam, facilis labore
+                    beatae nihil, modi architecto harum obcaecati?
+                </p>
+            </div>
+        </div>
+    )
+}
+
