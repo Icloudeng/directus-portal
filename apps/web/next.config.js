f694e6c7851157cde9285cdd705e0cc9a00ba9ec
paradoxe35 <35149259+paradoxe35@users.noreply.github.com>
2023-02-20 00:05:52 +0200

    feat: add sharp to improve standalone output

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,6 +2,7 @@ const { i18n } = require('./next-i18next.config');
 
 /** @type {import('next').NextConfig} */
 module.exports = {
+  output: 'standalone',
   i18n,
 
   eslint: {
--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -3,10 +3,7 @@ const { fontFamily } = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {
