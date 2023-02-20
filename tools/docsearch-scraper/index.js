f3ee4dc469e6e68c3a9e96e1721bb4ed8f7d9430
paradoxe35 <35149259+paradoxe35@users.noreply.github.com>
2023-02-20 11:28:06 +0200

    feat: docs scraper docker config

--- tools/docsearch-scraper/index.js
+++ tools/docsearch-scraper/index.js
@@ -3,11 +3,12 @@ import { spawn } from "node:child_process";
 import fs from "node:fs/promises";
 import which from "which";
 import config from "./config.json" assert { type: "json" };
+import chokidar from "chokidar";
 
 const ORIGIN = "http://host.docker.internal:3100";
+const IS_DOCKER = process.env.IS_DOCKER;
 
 (async () => {
-  const docker = await which("docker");
   const start_urls = config.start_urls;
 
   try {
@@ -15,7 +16,7 @@ const ORIGIN = "http://host.docker.internal:3100";
      * @type {{ url: string; variables: { lang: string[];};}[]}
      */
     const urls = JSON.parse(
-      await fs.readFile("./start-urls.json", { encoding: "utf-8" })
+      await fs.readFile("./meta/start-urls.json", { encoding: "utf-8" })
     );
 
     urls.forEach((path) => {
@@ -25,22 +26,63 @@ const ORIGIN = "http://host.docker.internal:3100";
     });
   } catch (_) {}
 
-  await cmd(
-    docker,
-    "run",
-    "--name=docsearch-scraper",
-    "--net=host",
-    "--rm",
-    "--add-host=host.docker.internal:host-gateway",
-    "--env-file=docker.env",
-    "-e",
-    `CONFIG=${JSON.stringify(config)}`,
-    "typesense/docsearch-scraper"
-  );
+  if (IS_DOCKER) {
+    const pipenv = await which("pipenv");
+
+    const pending = {
+      value: false,
+    };
+
+    const exec = async () => {
+      if (pending.value) {
+        return;
+      }
+
+      console.log("scraping.....");
+
+      pending.value = true;
+
+      await cmd(
+        {
+          ...(process.env || {}),
+          CONFIG: JSON.stringify(config),
+        },
+        [pipenv, "run", "python", "-m", "src.index"]
+      ).finally(() => (pending.value = false));
+    };
+
+    exec();
+    chokidar.watch("./meta").on("all", exec);
+  } else {
+    const docker = await which("docker");
+    await cmd(
+      {
+        ...(process.env || {}),
+      },
+      [
+        docker,
+        "run",
+        "--name=docsearch-scraper",
+        "--net=host",
+        "--rm",
+        "--add-host=host.docker.internal:host-gateway",
+        "--env-file=docker.env",
+        "-e",
+        `CONFIG=${JSON.stringify(config)}`,
+        "typesense/docsearch-scraper",
+      ]
+    );
+  }
 })();
 
-function cmd(...command) {
-  let p = spawn(command[0], command.slice(1), { cwd: process.cwd() });
+/**
+ *
+ * @param {NodeJS.ProcessEnv} env
+ * @param  {string[]} commands
+ * @returns
+ */
+function cmd(env, commands) {
+  let p = spawn(commands[0], commands.slice(1), { cwd: process.cwd(), env });
   return new Promise((resolveFunc) => {
     p.stdout.on("data", (x) => {
       process.stdout.write(x.toString());
