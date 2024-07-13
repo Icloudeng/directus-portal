7041038efecf397144fc614944a0bf6c025302fb
Paradoxe NGWASI <paradoxe.ngwasi@smatflow.com>
2024-07-13 10:27:46 +0000

    Improve layout consistency in simple cards

--- apps/web/src/components/sections/templates/st-simple-cards.tsx
+++ apps/web/src/components/sections/templates/st-simple-cards.tsx
@@ -6,7 +6,7 @@ import { useMut } from '@/cms/mut';
 
 export function ST_SimpleCardsFC({ items }: STemplates_Props<ST_SimpleCard>) {
   return (
-    <div className='flex flex-wrap'>
+    <div className='flex flex-wrap items-stretch'>
       {items.map((item) => {
         return <STSimpleCard key={item.item.id} item={item} />;
       })}
@@ -19,7 +19,7 @@ function STSimpleCard(props: { item: ST_SimpleCard }) {
 
   return (
     <div className='xl:w-1/3 md:w-1/2 p-4'>
-      <div className='border border-gray-200 p-6 rounded-lg'>
+      <div className='border border-gray-200 p-6 rounded-lg h-full'>
         <div className='w-10 h-10 inline-flex items-center justify-center rounded-full bg-primary-50 text-primary-500 mb-4'>
           {item.icon_svg && (
             <HasSvgText svgText={item.icon_svg} className='st_value_icon' />
