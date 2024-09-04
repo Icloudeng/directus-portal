57da166ab4c4ba4c215b7f986580ed6cecc7526a
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2024-09-04 20:57:33 +0000

    Adjust token TTL and enhance Directus client handling
    - Changed ACCESS_TOKEN_TTL from "15m" to "30s" in configuration files to enhance security by reducing the token lifespan.
    - Improved Directus client instantiation by adding auto-refresh and refresh-before-expiry configurations.
    - Ensured Directus client refreshes token if expired before returning a valid client instance.
    - These changes mitigate potential security risks and ensure the CMS operates with enhanced token management.

--- apps/web/src/cms/directus.ts
+++ apps/web/src/cms/directus.ts
@@ -6,16 +6,21 @@ import {
   DIRECTUS_URL,
 } from '@/app/constant/env';
 
-const directus = new Directus(DIRECTUS_URL);
+const directus = new Directus(DIRECTUS_URL, {
+  auth: {
+    autoRefresh: true,
+    msRefreshBeforeExpires: 1000,
+  },
+});
 
 export async function getDirectusClient() {
-  if (await directus.auth.token) return directus;
+  if (await directus.auth.token) {
+    return directus;
+  }
 
   if (!DIRECTUS_EMAIL || !DIRECTUS_PASSWORD) {
     throw new Error('Directus Admin credentials must be provided!');
   }
-
-  directus.auth.autoRefresh = true;
 
   await directus.auth.login({
     email: DIRECTUS_EMAIL,
--- tools/docs-generator/src/cms/directus.ts
+++ tools/docs-generator/src/cms/directus.ts
@@ -1,16 +1,23 @@
 import { Directus } from "@directus/sdk";
 import { DIRECTUS_HOST, DIRECTUS_EMAIL, DIRECTUS_PASSWORD } from "../constants";
 
-const directus = new Directus(DIRECTUS_HOST);
+const directus = new Directus(DIRECTUS_HOST, {
+  auth: {
+    autoRefresh: true,
+    msRefreshBeforeExpires: 1000,
+  },
+});
 
 export async function getDirectusClient() {
-  if (await directus.auth.token) return directus;
+  await directus.auth.refreshIfExpired();
+
+  if (await directus.auth.token) {
+    return directus;
+  }
 
   if (!DIRECTUS_EMAIL || !DIRECTUS_PASSWORD) {
     throw new Error("Directus Admin credentials must be provided!");
   }
-
-  directus.auth.autoRefresh = true;
 
   await directus.auth.login({
     email: DIRECTUS_EMAIL,
